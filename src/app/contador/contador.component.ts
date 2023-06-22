1153d893a97eb28a1d09cb6b188da252b69cab0a
Neonx <fabiangomes2015@gmail.com>
2023-06-22 18:06:17 -0500

    Adding an interface

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../interfaces/persona';
 @Component({
   selector: 'app-contador',
   templateUrl: './contador.component.html',
   styleUrls: ['./contador.component.scss']
 })
-export class ContadorComponent {
+export class ContadorComponent implements OnInit {
 
+  ngOnInit(): void {
+  }
+
+
+  // nombre:string = "fernando";
+
+  // persona:Persona ={
+  //   nombre: "fernando",
+  //   edad:23
+  // }
 }
