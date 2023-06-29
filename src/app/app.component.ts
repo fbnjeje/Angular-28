3a3946c94fbefaa52d861ff2033b01ca294ab733
fbn <fabiangomes2015@gmail.com>
2023-06-29 17:43:30 -0500

    creating a list with delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,44 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Curso angular ';
+  // title = 'Curso angular ';
 
 
-  hola = [
-    "pedro",
-    "laura",
-    "valentina",
-    "ferxxxo"
-  ]
-  Activated = true;
+  // hola = [
+  //   "pedro",
+  //   "laura",
+  //   "valentina",
+  //   "ferxxxo"
+  // ]
+  // Activated = true;
 
 
-  press(){
-    this.Activated = false;
-  }
-  dontpress(){
-    this.Activated = true;
-  }
+  // press(){
+  //   this.Activated = false;
+  // }
+  // dontpress(){
+  //   this.Activated = true;
+  // }
 
 
-  name:string = "Ray Ray";
-  age!:number;
-  address:{
-    street:string;
-    city:string;
-  }
+  // name:string = "Ray Ray";
+  // age!:number;
+  // address:{
+  //   street:string;
+  //   city:string;
+  // }
   
-  hobbies!: string[];
+  // hobbies!: string[];
 
-  constructor(){
-    this.age = 29;
-    this.address = {
-      street: '211b baker street',
-      city: "London"
-    };
+  // constructor(){
+  //   this.age = 29;
+  //   this.address = {
+  //     street: '211b baker street',
+  //     city: "London"
+  //   };
 
-    this.hobbies = ['nada','escribir','comer'];
+  //   this.hobbies = ['nada','escribir','comer'];
+  // }
+
+
+   users:string[] = ['cameron','carlos','ferxxo'];
+  // sayHola(){
+  //   alert('hello')
+  // }
+
+
+  deleteUser(user:any){
+      for(let i = 0 ; i<this.deleteUser.length; i++){
+        if(user === this.users[i]){
+          this.users.splice(i,1)
+        }
+      }
   }
-
 }
 
